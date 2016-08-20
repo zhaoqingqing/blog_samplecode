16477477655f030c4aba983d518a85bf812e5e82
zhaoqingqing <zhaoqignqing@gmail.com>
2016-08-20 16:58:26 +0800

    add SimpleJson.cs;add DetectMonoVersion.cs

---
+++ unity-framework/DetectMonoVersion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Reflection;
+
+//show what unity use mono vertion
+public class DetectMonoVersion : MonoBehaviour {
+	void Start()
+	{
+		Type type = Type.GetType("Mono.Runtime");
+		if (type != null)
+		{
+			MethodInfo info = type.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static);
+			
+			if (info != null)
+				Debug.Log(info.Invoke(null, null));
+		}
+      //Debug.Log(System.Enviroment.Version);//net version?
+	}
+}
