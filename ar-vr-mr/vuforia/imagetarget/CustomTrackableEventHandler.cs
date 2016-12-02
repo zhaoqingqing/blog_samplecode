0a561f7081eb47efa367fd2628852042a1e3f5e3
qingqing-zhao <zhaoqignqing@gmail.com>
2016-12-02 10:26:10 +0800

    imagetarget 多图识别Test

---
+++ ar-vr-mr/vuforia/imagetarget/CustomTrackableEventHandler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+/// <summary>
+/// Detail		:  仅仅是对DefaultTrackableEventHandler添加了OnFound和OnLost事件
+/// Author		:  qingqing-zhao(569032731@qq.com)
+/// CreateTime  :  #CreateTime#
+/// </summary>
+public class CustomTrackableEventHandler : MonoBehaviour,
+                                                ITrackableEventHandler
+{
+    private TrackableBehaviour _mTrackableBehaviour;
+    //识别成功
+    public delegate void OnFoundEvent(string trackableName);
+    public static event OnFoundEvent OnFound;
+    //丢失
+    public delegate void OnLostEvent(string trackableName);
+    public static event OnLostEvent OnLost;
+
+    void Start()
+    {
+        _mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (_mTrackableBehaviour)
+        {
+            _mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    /// <summary>
+    /// 识别状态改变调用
+    /// </summary>
+    /// <param name="previousStatus"></param>
+    /// <param name="newStatus"></param>
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+
+    /// <summary>
+    /// 识别到
+    /// </summary>
+    private void OnTrackingFound()
+    {
+        Debug.Log("Trackable " + _mTrackableBehaviour.TrackableName + " found");
+        if (OnFound != null) OnFound(_mTrackableBehaviour.TrackableName);
+    }
+
+
+    /// <summary>
+    /// 丢失
+    /// </summary>
+    private void OnTrackingLost()
+    {
+        Debug.Log("Trackable " + _mTrackableBehaviour.TrackableName + " lost");
+        if (OnLost != null) OnLost(_mTrackableBehaviour.TrackableName);
+    }
+
+}
---
+++ ar-vr-mr/vuforia/imagetarget/TrackableTest.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Vuforia;
+
+/// <summary>
+/// Detail		:  多个ImageTarget识别的尝试
+/// Author		:  qingqing-zhao(569032731@qq.com)
+/// CreateTime  :  #CreateTime#
+/// </summary>
+public class TrackableTest : MonoBehaviour
+{
+
+    private Dictionary<string, TrackableBehaviour> Name2TrackDict = new Dictionary<string, TrackableBehaviour>();
+    public float ImageOffsetX = 1.45f;
+
+    // Use this for initialization
+    void Start()
+    {
+        //获取所有的可识别对象
+        IEnumerable<TrackableBehaviour> trackableBehaviours = TrackerManager.Instance.GetStateManager().GetTrackableBehaviours();
+        if (trackableBehaviours != null)
+        {
+            Debug.LogFormat("共有 {0} 个识别图", trackableBehaviours.Count());
+            Vector3 pos = new Vector3(0, 0, 0);
+
+            foreach (TrackableBehaviour trackableBehaviour in trackableBehaviours)
+            {
+                if (trackableBehaviour == null) continue;
+                trackableBehaviour.gameObject.AddComponent<CustomTrackableEventHandler>();
+
+                trackableBehaviour.transform.localPosition = pos;
+                trackableBehaviour.gameObject.layer = 8;
+
+                ImageTargetBehaviour targetBehaviour = trackableBehaviour.gameObject.GetComponent<ImageTargetBehaviour>();
+                //NOTE 不建议手动设置ImageTargetBehaviour的Size，看实际情况
+                //NOTE 多个ImageTarget之间要有间隙,可以理解为平铺开来
+                pos += new Vector3(targetBehaviour.GetSize().y, pos.y, pos.z);
+                ImageOffsetX = targetBehaviour.GetSize().y;
+                trackableBehaviour.gameObject.name = string.Concat("ImageTarget_", trackableBehaviour.TrackableName);
+                if (!Name2TrackDict.ContainsKey(trackableBehaviour.TrackableName))
+                {
+                    Name2TrackDict.Add(trackableBehaviour.TrackableName, trackableBehaviour);
+                }
+            }
+            Debug.Log("所有识别图都已生成完成");
+        }
+
+        CustomTrackableEventHandler.OnFound += CustomTrackableEventHandler_OnFound;
+        CustomTrackableEventHandler.OnLost += CustomTrackableEventHandler_OnLost;
+    }
+
+    public void CustomTrackableEventHandler_OnFound(string trackableName)
+    {
+        if (Name2TrackDict.ContainsKey(trackableName))
+        {
+            TrackableBehaviour trackableBehaviour = Name2TrackDict[trackableName];
+            if (trackableBehaviour != null)
+            {
+                //TODO OnFund
+            }
+        }
+    }
+
+    private void CustomTrackableEventHandler_OnLost(string trackableName)
+    {
+        if (Name2TrackDict.ContainsKey(trackableName))
+        {
+            //Fix: 重设位置到初始值 否则多次丢失之后位置会发生错乱
+            TrackableBehaviour trackableBehaviour = Name2TrackDict[trackableName];
+            //ID 就是图片在xml中的index,从1开始
+            var posX = trackableBehaviour.Trackable.ID * ImageOffsetX;
+            trackableBehaviour.transform.localPosition = new Vector3(posX, 0, 0);
+            trackableBehaviour.transform.localRotation = Quaternion.identity;
+        }
+
+    }
+
+    void OnDestory()
+    {
+        CustomTrackableEventHandler.OnFound -= CustomTrackableEventHandler_OnFound;
+        CustomTrackableEventHandler.OnLost -= CustomTrackableEventHandler_OnLost;
+    }
+
+}
