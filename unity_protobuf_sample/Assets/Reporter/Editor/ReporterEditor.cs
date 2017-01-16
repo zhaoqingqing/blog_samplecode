47940ae0503ffd59c07669313ff0aa07504309b1
qingqing-zhao <zhaoqignqing@gmail.com>
2017-01-16 17:59:27 +0800

    更新 ReporterEditor.cs

--- unity_protobuf_sample/Assets/Reporter/Editor/ReporterEditor.cs
+++ unity_protobuf_sample/Assets/Reporter/Editor/ReporterEditor.cs
@@ -20,36 +20,37 @@ public class MyAssetModificationProcessor : UnityEditor.AssetModificationProcess
 		//reporterObj.AddComponent<TestReporter>();
 
 		reporter.images = new Images();
-		reporter.images.clearImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/clear.png", typeof(Texture2D));
-		reporter.images.collapseImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/collapse.png", typeof(Texture2D));
-		reporter.images.clearOnNewSceneImage= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/clearOnSceneLoaded.png", typeof(Texture2D));
-		reporter.images.showTimeImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/timer_1.png", typeof(Texture2D));
-		reporter.images.showSceneImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/UnityIcon.png", typeof(Texture2D));
-		reporter.images.userImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/user.png", typeof(Texture2D));
-		reporter.images.showMemoryImage 	= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/memory.png", typeof(Texture2D));
-		reporter.images.softwareImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/software.png", typeof(Texture2D));
-		reporter.images.dateImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/date.png", typeof(Texture2D));
-		reporter.images.showFpsImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/fps.png", typeof(Texture2D));
-		reporter.images.showGraphImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/.png", typeof(Texture2D));
-		reporter.images.graphImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/chart.png", typeof(Texture2D));
-		reporter.images.infoImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/info.png", typeof(Texture2D));
-		reporter.images.searchImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/search.png", typeof(Texture2D));
-		reporter.images.closeImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/close.png", typeof(Texture2D));
-		reporter.images.buildFromImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/buildFrom.png", typeof(Texture2D));
-		reporter.images.systemInfoImage 	= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/ComputerIcon.png", typeof(Texture2D));
-		reporter.images.graphicsInfoImage 	= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/graphicCard.png", typeof(Texture2D));
-		reporter.images.backImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/back.png", typeof(Texture2D));
-		reporter.images.cameraImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/.png", typeof(Texture2D));
-		reporter.images.logImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/log_icon.png", typeof(Texture2D));
-		reporter.images.warningImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/warning_icon.png", typeof(Texture2D));
-		reporter.images.errorImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/error_icon.png", typeof(Texture2D));
-		reporter.images.barImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/bar.png", typeof(Texture2D));
-		reporter.images.button_activeImage 	= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/button_active.png", typeof(Texture2D));
-		reporter.images.even_logImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/even_log.png", typeof(Texture2D));
-		reporter.images.odd_logImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/odd_log.png", typeof(Texture2D));
-		reporter.images.selectedImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/selected.png", typeof(Texture2D));
+	    string imageRootPath = "Assets/Reporter/Images/";
+		reporter.images.clearImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"clear.png", typeof(Texture2D));
+		reporter.images.collapseImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"collapse.png", typeof(Texture2D));
+		reporter.images.clearOnNewSceneImage= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"clearOnSceneLoaded.png", typeof(Texture2D));
+		reporter.images.showTimeImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"timer_1.png", typeof(Texture2D));
+		reporter.images.showSceneImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"UnityIcon.png", typeof(Texture2D));
+		reporter.images.userImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"user.png", typeof(Texture2D));
+		reporter.images.showMemoryImage 	= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"memory.png", typeof(Texture2D));
+		reporter.images.softwareImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"software.png", typeof(Texture2D));
+		reporter.images.dateImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"date.png", typeof(Texture2D));
+		reporter.images.showFpsImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"fps.png", typeof(Texture2D));
+		reporter.images.showGraphImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +".png", typeof(Texture2D));
+		reporter.images.graphImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"chart.png", typeof(Texture2D));
+		reporter.images.infoImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"info.png", typeof(Texture2D));
+		reporter.images.searchImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"search.png", typeof(Texture2D));
+		reporter.images.closeImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"close.png", typeof(Texture2D));
+		reporter.images.buildFromImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"buildFrom.png", typeof(Texture2D));
+		reporter.images.systemInfoImage 	= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"ComputerIcon.png", typeof(Texture2D));
+		reporter.images.graphicsInfoImage 	= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"graphicCard.png", typeof(Texture2D));
+		reporter.images.backImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"back.png", typeof(Texture2D));
+		reporter.images.cameraImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +".png", typeof(Texture2D));
+		reporter.images.logImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"log_icon.png", typeof(Texture2D));
+		reporter.images.warningImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"warning_icon.png", typeof(Texture2D));
+		reporter.images.errorImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"error_icon.png", typeof(Texture2D));
+		reporter.images.barImage 			= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"bar.png", typeof(Texture2D));
+		reporter.images.button_activeImage 	= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"button_active.png", typeof(Texture2D));
+		reporter.images.even_logImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"even_log.png", typeof(Texture2D));
+		reporter.images.odd_logImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"odd_log.png", typeof(Texture2D));
+		reporter.images.selectedImage 		= (Texture2D)AssetDatabase.LoadAssetAtPath(imageRootPath +"selected.png", typeof(Texture2D));
 
-		reporter.images.reporterScrollerSkin = (GUISkin)AssetDatabase.LoadAssetAtPath("Assets/Reporter/Images/reporterScrollerSkin.guiskin", typeof(GUISkin));
+		reporter.images.reporterScrollerSkin = (GUISkin)AssetDatabase.LoadAssetAtPath(imageRootPath+"reporterScrollerSkin.guiskin", typeof(GUISkin));
 
 	}
 	[InitializeOnLoad]
