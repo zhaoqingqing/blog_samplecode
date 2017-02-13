c0c68207112682fad988a877589bb128ad7e59ce
qingqing-zhao <zhaoqignqing@gmail.com>
2017-02-13 17:40:55 +0800

    Editor下创建txt和lua文件

---
+++ unity_helper/Editor/CreateFileEditor.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Unity Editor 下右键创建文本类文件
+/// </summary>
+public class CreateFileEditor : Editor
+{
+    [MenuItem("Assets/Create/Lua File")]
+    static void CreateLuaFile()
+    {
+        CreateFile("lua");
+    }
+
+    [MenuItem("Assets/Create/Text File")]
+    static void CreateTextFile()
+    {
+        CreateFile("txt");
+    }
+
+    /// <summary>
+    /// 创建文件类的文件
+    /// </summary>
+    /// <param name="fileEx"></param>
+    static void CreateFile(string fileEx)
+    {
+        //获取当前所选择的目录（相对于Assets的路径）
+        var selectPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+        var path = Application.dataPath.Replace("Assets", "") + "/";
+        var newFileName = "new_" + fileEx + "." + fileEx;
+        var newFilePath = selectPath + "/" + newFileName;
+        var fullPath = path + newFilePath;
+
+        //简单的重名处理
+        if (File.Exists(fullPath))
+        {
+            var newName = "new_" + fileEx + "-" + UnityEngine.Random.Range(0, 100) + "." + fileEx;
+            newFilePath = selectPath + "/" + newName;
+            fullPath = fullPath.Replace(newFileName, newName);
+        }
+
+        //如果是空白文件，编码并没有设成UTF-8
+        File.WriteAllText(fullPath, "-- test", Encoding.UTF8);
+
+        AssetDatabase.Refresh();
+
+        //选中新创建的文件
+        var asset = AssetDatabase.LoadAssetAtPath(newFilePath, typeof(Object));
+        Selection.activeObject = asset;
+    }
+}
