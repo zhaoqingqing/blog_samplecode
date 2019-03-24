5048b1c73873c96e26ca8727b4648f3940e1be8a
qingqing-zhao <zhaoqignqing@gmail.com>
2019-03-24 19:23:14 +0800

    vs新建文件模版和插入代码片段

---
+++ workflow-tools/vs(visual studio)/Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// 作者：赵青青 (https://www.cnblogs.com/zhaoqingqing)
+    /// 时间：$time$
+    /// 说明：
+    /// </summary>
+    class $safeitemrootname$
+    {
+    }
+}
---
+++ workflow-tools/vs(visual studio)/vs新建文件模版.md
@@ -0,0 +1,12 @@
+### 新建文件模版
+
+以VS2017专业版为例：把Class.cs文件放置在这个目录下：
+C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\ItemTemplates\CSharp\Code\2052\Class
+
+### 代码片段
+
+以VS2017专业版为例：把author.snippet文件放置在这个目录下：
+
+C:\Users\Administrator\Documents\Visual Studio 2017\Code Snippets\Visual C#\My Code Snippets
+
+或在VS中选择 工具 - 代码片段管理器 - 选择CSharp ，复制路径，打开，粘贴进去。
