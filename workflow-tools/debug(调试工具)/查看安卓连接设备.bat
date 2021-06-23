e131ba6f39982919eac263996b37f40525324e56
qingqing-zhao <zhaoqignqing@gmail.com>
2021-06-23 11:35:41 +0800

    add @echo off

--- workflow-tools/debug(调试工具)/查看安卓连接设备.bat
+++ workflow-tools/debug(调试工具)/查看安卓连接设备.bat
@@ -1,8 +1,10 @@
+@echo off
 REM 安卓模拟器的安装路径
 cd c:\
 cd C:\Program Files (x86)\MuMu\emulator\nemu\EmulatorShell\
 ::cd C:\Program Files (x86)\Droid4X
 REM 如果非安卓模拟器，可以注释上面行
 REM adb kill-server
+@echo on
 adb devices
 PAUSE
