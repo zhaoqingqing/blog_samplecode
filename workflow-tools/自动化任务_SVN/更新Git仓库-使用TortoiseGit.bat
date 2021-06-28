a9b9d8634f3f758bf689880c8944ed05ef4a3fc1
qingqing-zhao <zhaoqignqing@gmail.com>
2021-06-28 09:16:27 +0800

    添加 echo off

--- workflow-tools/自动化任务_SVN/更新Git仓库-使用TortoiseGit.bat
+++ workflow-tools/自动化任务_SVN/更新Git仓库-使用TortoiseGit.bat
@@ -2,8 +2,10 @@ rem TortoiseGit命令行更新多个不同的git库,每次更新会弹出窗口分支确认窗口
 
 @echo off
 @echo ================自动更新git Start===============
+
+@echo off
 rem tortoisegit安装目录
-SET svn_home=C:\Program Files\TortoiseGit\bin
+SET git_home=C:\Program Files\TortoiseGit\bin
 
 rem git repo目录(请修改对应的目录)
 SET blog_samplecode=E:\Code\blog_samplecode
@@ -11,7 +13,9 @@ SET code_snippetE:\Code\code_snippetE
 SET unity_study=E:\Code\unity_study
 SET python_study=E:\Code\python_study
 
+
 rem 执行tortoisegit更新命令,更新对话框不自动关闭
-"%svn_home%"\TortoiseGitProc.exe/command:pull /path:"%blog_samplecode%*%code_snippet%" /notempfile /closeonend:0
+@echo on
+"%git_home%"\TortoiseGitProc.exe/command:pull /path:"%blog_samplecode%*%code_snippet%" /notempfile /closeonend:0
 
 echo ==============自动更新git Finish==============
--- workflow-tools/自动化任务_SVN/更新Git仓库-使用git命令行.bat
+++ workflow-tools/自动化任务_SVN/更新Git仓库-使用git命令行.bat
@@ -3,15 +3,17 @@ rem Git命令行更新多个不同的git库
 @echo off
 @echo ================自动更新git Start===============
 
-
+@echo off
 rem git repo目录(请修改对应的目录)
 SET blog_samplecode=E:\Code\blog_samplecode
 SET code_snippet=E:\Code\code_snippet
 SET unity_study=E:\Code\unity_study
 SET python_study=E:\Code\python_study
 
+
 cd e:
 cd %blog_samplecode%
+@echo on
 git pull
 
 cd %code_snippet%
