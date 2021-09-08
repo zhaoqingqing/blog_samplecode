fcd85478a9a1831362d6a4749cc65f2b00f13946
qingqing.zhao <zhaoqignqing@gmail.com>
2021-09-08 11:22:44 +0800

    split python2 env to single script file

--- build-tools/cnblog2.py
+++ build-tools/cnblog2.py
@@ -1,7 +1,5 @@
-#! python3
-#! /usr/bin/env python
-# coding=utf-8
-
+#! python2
+# -*- coding: UTF-8 -*-
 """
 感谢nickchen121的脚本，开源地址(https://github.com/nickchen121/cnblogs_automatic_blog_uploading)
 原理：使用python 基于 xmlrpc 调用metaweblog API 发送内容到博客园
@@ -9,9 +7,11 @@
 博客园限制，不能连续不断的发表，所以使用threading间隔60S发一次。
 NOTE: 文章从目录中发布正式或草稿后，会删除原文件，所以需要对源md文件进行备份
 cnblog2.py work in python2 , cnblog.py work in python3
-TODO 把py脚本中的空格换成Tab
+	1. 61行打开文件会报错
+	2.json.dump({},f)报错：TypeError: write() argument 1 must be unicode, not str
 """
-import xmlrpc.client as xmlrpclib
+
+import  xmlrpclib
 import glob
 import os
 import sys
@@ -19,6 +19,9 @@ import json
 import time
 import datetime
 import ssl
+import io
+from io import open
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # 使用相对路径
@@ -44,6 +47,20 @@ str  | usr, 登录用户名
 str  | passwd, 登录密码
 '''
 
+import codecs
+import warnings
+
+#在py2中兼容py3的写法
+def open(file, mode='r', buffering=-1, encoding=None,
+		 errors=None, newline=None, closefd=True, opener=None):
+	if newline is not None:
+		warnings.warn('newline is not supported in py2')
+	if not closefd:
+		warnings.warn('closefd is not supported in py2')
+	if opener is not None:
+		warnings.warn('opener is not supported in py2')
+	return codecs.open(filename=file, mode=mode, encoding=encoding,
+					   errors=errors, buffering=buffering)
 
 def exist_cfg():
 	'''
@@ -61,7 +78,9 @@ def exist_cfg():
 				return False
 	except:
 		with open(path_cfgfile, "w", encoding="utf-8") as f:
-			json.dump({}, f)
+			t1=f.read()
+			t2=t1.decode('utf-8')
+			json.dump({}, f)#f.read().decode('utf-8'))
 			return False
 
 
@@ -111,23 +130,26 @@ def get_cfg():
 		passwd = cfg["passwd"]
 		server = xmlrpclib.ServerProxy(url)
 		mwb = server.metaWeblog
-		print("初始化 blogid:{0},usr:{1},recentnum:{2},url:{3}".format(blogid, usr, recentnum, url))
-		#储存博客中文章标题对应的postid
-		recentPost = mwb.getRecentPosts(blogid, usr, passwd, recentnum)
-		for post in recentPost:
-			#print("最近发布文章为:",post["title"])
-			# 1.把datetime转成字符串
-			dt = post["dateCreated"]
-			# post["dateCreated"] = dt.strftime("%Y%m%dT%H:%M:%S")
-			post["dateCreated"] = dt.__str__()
-			# 2.把字符串转成datetime
-			# datetime.datetime.strptime(st, "%Y%m%dT%H:%M:%S")
-			# datetime.datetime.fromisoformat(str)
-			title2id[post["title"]] = post["postid"]
-		# 保存最近发布内容到文件中，文件名：20160320-114539
-		filename = time.strftime("%Y%m%d-%H%M%S", time.localtime())
-		with open(path_backup + filename + ".json", "w", encoding="utf-8") as f:
-			json.dump(recentPost, f, indent=4 ,ensure_ascii=False)
+		print ("init blogid:{},usr:{},recentnum:{},url:{}".format(blogid, usr, recentnum, url))
+
+#在python2中弃用掉保存最近发表文章,因为open失败了
+def save_last():
+	#储存博客中文章标题对应的postid
+	recentPost = mwb.getRecentPosts(blogid, usr, passwd, recentnum)
+	for post in recentPost:
+		#print("最近发布文章为:",post["title"])
+		# 1.把datetime转成字符串
+		dt = post["dateCreated"]
+		# post["dateCreated"] = dt.strftime("%Y%m%dT%H:%M:%S")
+		post["dateCreated"] = dt.__str__()
+		# 2.把字符串转成datetime
+		# datetime.datetime.strptime(st, "%Y%m%dT%H:%M:%S")
+		# datetime.datetime.fromisoformat(str)
+		title2id[post["title"]] = post["postid"]
+	# 保存最近发布内容到文件中，文件名：20160320-114539
+	# filename = time.strftime("%Y%m%d-%H%M%S", time.localtime())
+	# with open(path_backup + filename + ".json", "w", encoding="utf-8") as f:
+		# json.dump(recentPost, f, indent=4 ,ensure_ascii=False)
 
 def newPost(blogid, usr, passwd, post, publish):
 	while True:
@@ -136,12 +158,13 @@ def newPost(blogid, usr, passwd, post, publish):
 			break
 		except BaseException as e:
 			#发布失败
-			print("post error:{}".format(e))
+			print ("newPost error ,",e) #Request from client does not contain valid XML
 			time.sleep(5)
 	return postid
 
 #已经发布的不会重复发布，且未发布目录内容如果为空，会报TypeError: cannot unpack non-iterable NoneType object
 def post_art(path, publish=True):
+	print ("dopost 1,path={} ,publish={}".format(path,publish))
 	title = os.path.basename(path)  # 获取文件名做博客文章标题
 	[title, _] = os.path.splitext(title)  # 去除扩展名
 	with open(path, "r", encoding="utf-8") as f:
@@ -155,10 +178,10 @@ def post_art(path, publish=True):
 			# 所以先删除这个文章
 			# if title in title2id.keys():
 			#     server.blogger.deletePost(appkey, title2id[title], usr, passwd, True)
-
+			print ("dopost 2-1")
 			if title not in title2id.keys():
 				post["categories"].append('[随笔分类]unpublished')  # 标记未发布
-				postid = newPost(blogid, usr, passwd, post, publish)
+				postid = newPost(blogid, usr, passwd, post, publish) #fuck! py2下此接口一直没回包
 
 				print("New Draft:[title=%s][postid=%s][publish=%r]" %(title, postid, publish))
 			else:
@@ -175,6 +198,7 @@ def post_art(path, publish=True):
 
 			return (title, postid, publish)
 		else:  # 发布
+			print ("dopost 2-2")
 			if title in title2id.keys():  # 博客里已经存在这篇文章
 				postid = title2id[title]
 				succcess = mwb.editPost(title2id[title], usr, passwd, post, publish)
@@ -214,7 +238,9 @@ def send_draft():
 	'''
 	发布到草稿箱
 	'''
-	for mdfile in glob.glob(path_draft + "*.md"):
+	for mdfile in glob.glob(path_draft + "*.md"):#py2环境在pycharm中输出中文文件名为乱码，但在powershell下又是正常的
+		# print mdfile
+		# print unicode(mdfile,'utf-8')
 		title, postid, publish = post_art(mdfile, False)
 
 def send_article():
@@ -226,7 +252,13 @@ def send_article():
 		title_postid_dict[title] = postid
 
 if __name__ == "__main__":
-	# 创建路径
+	default_encoding = 'utf-8'
+	if sys.getdefaultencoding() != default_encoding:
+		reload(sys)
+		sys.setdefaultencoding(default_encoding)
+		print ("set encoding to utf-8")
+
+	# 创建路径 TODO py2不会创建新目录
 	for path in [path_publish, path_draft, path_backup]:
 		if not os.path.exists(path):
 			os.makedirs(path)
@@ -243,7 +275,7 @@ if __name__ == "__main__":
 
 		# 配置用户参数
 		if len(sys.argv) > 1:
-			print("do action:{}".format(sys.argv[1]))
+			print ("do action:{}".format(sys.argv[1]))
 			if sys.argv[1] == "download":
 				download_art()
 			elif sys.argv[1] == "config":
@@ -253,15 +285,18 @@ if __name__ == "__main__":
 				send_draft()
 			elif sys.argv[1] == "publish":
 				send_article()
+			else:
+				send_draft()
+				# send_article()
 		else:#开发环境下调试
-			print("do action dev env")
+			print ("do action dev env")
 			send_draft()
-			send_article()
-	except BaseException as e:
-		print("error:{}".format(e))
-		#保存操作日志
-		with open('title_postid.json', 'w', encoding='utf8') as fw:
-			json.dump(title_postid_dict, fw ,ensure_ascii=False)
+			# send_article()
 
-	with open('title_postid.json', 'w', encoding='utf8') as fw:
-		json.dump(title_postid_dict, fw , ensure_ascii=False)
+	except BaseException as e:
+		#保存操作日志
+		# with open('title_postid.json', 'w', encoding='utf8') as fw:
+		#     json.dump(title_postid_dict, fw ,ensure_ascii=False)
+		print ("error:",e)
+	# with open('title_postid.json', 'w', encoding='utf8') as fw:
+	#     json.dump(title_postid_dict, fw , ensure_ascii=False)
